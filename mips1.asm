6826b7929dd1ddf758f58304707fc20a68304e5f
It's me Abel <abel97.ag@gmail.com>
2023-09-02 14:48:24 +0200

    Update mips1.asm
    la till globala variabler och en del av skriv metoden

--- mips1.asm
+++ mips1.asm
@@ -1,4 +1,7 @@
 .data
+antal: .word 10
+vek: .word 4,5,2,2,1,6,7,9,5,10
+newline: .asciiz "\n"
 
 
 
@@ -7,6 +10,36 @@
 
 
 .text
-.globl Main
-Main: 
+.globl main
+main:
+
+
+skriv:
+#förbreda parametrarna
+#s0 till v[], s1 till size
+addi $t0, $zero, 0 #i
+
+addi $v0, $zero, 4
+move $a0, newline
+syscall
+
+
+for:
+   bge $t0, $s1, endfor
+   addi $t0, $t0, 1
+   sll $t2, $t0, 2
+   
+   lw $t1, 0($t)
+   
+   addi $v0, $zero, 4
+   move $a0, newline
+
+j for
+endfor: 
+
+partition:
+
+
+
+quicksort:
 
