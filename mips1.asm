a4b13468edde64f111c18ff0c6e4b65a5b7bdb2a
It's me Abel <abel97.ag@gmail.com>
2023-09-03 21:28:00 +0200

    skriv funktionen är klar nu + Mars4_5, som är själva programmet som används för kod skrivning och exekvering

--- mips1.asm
+++ mips1.asm
@@ -2,6 +2,7 @@
 antal: .word 10
 vek: .word 4,5,2,2,1,6,7,9,5,10
 newline: .asciiz "\n"
+space: .asciiz ", "
 
 
 
@@ -13,30 +14,47 @@ newline: .asciiz "\n"
 .globl main
 main:
 
+lw $a0, antal
+la $a1, vek
+jal skriv
+
+li $v0, 10
+syscall
 
 skriv:
 #förbreda parametrarna
-#s0 till v[], s1 till size
+#a0 till antal, a1 till v[]
 addi $t0, $zero, 0 #i
+move $t3, $a0	#flyttar Antal till t3 registern
+move $t4, $a1	#flyttar Vek till t4 registern
+
 
 addi $v0, $zero, 4
-move $a0, newline
+la $a0, newline
 syscall
-
-
 for:
-   bge $t0, $s1, endfor
+   bge $t0, $t3, endfor
+	li $v0, 1
+	lw $a0, ($t4)
+	syscall
+   
+   	li $v0, 4
+   	la $a0, space
+   	syscall
+   	
+   
+   addi $t4, $t4, 4
    addi $t0, $t0, 1
-   sll $t2, $t0, 2
    
-   lw $t1, 0($t)
-   
-   addi $v0, $zero, 4
-   move $a0, newline
+
 
 j for
 endfor: 
-
+	
+	addi $v0, $zero, 4
+   	la $a0, newline
+   	syscall
+   	jr $ra
 partition:
 
 
