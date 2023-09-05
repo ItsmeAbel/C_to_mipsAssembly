443ff157f7cf962d8c83495cb977ff4bd9dbacf1
Abel <abel97.ag@gmail.com>
2023-09-05 20:16:46 +0200

    Fixed some errors
    Found some errors in the code. Code still not working properly. Has logical errors

--- mips1.asm
+++ mips1.asm
@@ -7,8 +7,8 @@ space: .asciiz ", "
 .text
 .globl main
 main:
-	la $a1, vek
-	lw $a0, antal
+	la $a0, vek
+	lw $a1, antal
 	jal skriv
 
 	la $a0, vek
@@ -17,21 +17,19 @@ main:
 	li $a2, 0 
 	jal quicksort
 
-	la $a1, vek
-	lw $a0, antal
+	la $a0, vek
+	lw $a1, antal
 	jal skriv
 
 	li $v0, 10
 	syscall
-
+#---------------------------------Skriv-----------------------------------------------------
 skriv:
 #förbreda parametrarna
 #a0 till antal, a1 till v[]
 	addi $t0, $zero, 0 #i
-	move $t3, $a0	#flyttar Antal till t3 registern
-	move $t4, $a1	#flyttar Vek till t4 registern
-
-
+	move $t3, $a1	#flyttar Antal till t3 registern
+	move $t4, $a0	#flyttar Vek till t4 registern
 
 	addi $v0, $zero, 4
 	la $a0, newline
@@ -50,10 +48,10 @@ for:
    	syscall
    	
    #går till nästa adress i arrayen
-   addi $t4, $t4, 4
-   addi $t0, $t0, 1 #ökar i med 1
+   	addi $t4, $t4, 4
+   	addi $t0, $t0, 1 #ökar i med 1
    
-j for
+	j for
 
 endfor: 
 	#skriver ut new line
@@ -61,6 +59,7 @@ endfor:
    	la $a0, newline
    	syscall
    	jr $ra
+#-----------------------------Partition-------------------------------------   	
 partition:
 
 	li $s1, 0 #pivot
@@ -69,9 +68,9 @@ partition:
 	li $s4, 0 #temp
 	
 	sll $t0, $a1, 2
-	add $t0, $a0, $t0
+	addu $t0, $a0, $t0
 	lw $s1, 0($t0) #pivot = v[a]
-	addi $s2, $a1, 1 #lower = a + 1;
+	addu $s2, $a1, 1 #lower = a + 1;
 	move $s3, $a2 #upper = b;
 	
 do: 
@@ -82,7 +81,7 @@ while1:
         lw $t3, 0($t0) #v[lower]
 	bgt $t3, $s1, endwhile1 #v[lower] > pivot
 	bgt $s2, $s3, endwhile1 #lower > upper
-	 addi $s2, $s2, 1 #lower = lower + 1
+	 addu $s2, $s2, 1 #lower = lower + 1
 	j while1
 
 endwhile1:
@@ -92,75 +91,88 @@ while2:
 	lw $t3, 0($t1)
 	ble $t3, $s1, endwhile2 #v[upper] <= pivot
 	bgt $s2, $s3, endwhile2 #lower <= upper
-	  addi $s3, $s3, -1
+	  addu $s3, $s3, -1
 	j while2
 endwhile2:
 
 	bgt $s2, $s3, endif0
 	
 	sll $t1, $s2, 2
-	add $t0, $a0, $t1 #v[lower]
+	addu $t0, $a0, $t1 #v[lower]
 	lw $s4, 0($t0)
 	
 	sll $t1, $s3, 2
-	add $t2, $a0, $t1 #v[upper]
-	sw $t2, 0($t0)
-	sw $s4, 0($t2)
+	addu $t2, $a0, $t1 #v[upper]
+	lw $t6, 0($t2)
+	sll $t0, $s2, 2
+	addu $t0, $a0, $t0
+	sw $t6, 0($t0)
 	
-	addi $s2, $s2, 1 #lower = lower + 1
-	addi $s3, $s3, -1 #upper = upper - 1
+	sll $t6, $s3, 2
+	addu $t6, $a0, $t6
+	sw $s4, 0($t6)
+	
+	addu $s2, $s2, 1 #lower = lower + 1
+	addu $s3, $s3, -1 #upper = upper - 1
 endif0:
 
 	ble $s2, $s3, do	
 	
 	sll $t0, $s3, 2
-        add $t1, $a0, $t0 #adressen till v[upper]
+        addu $t1, $a0, $t0 #adressen till v[upper]
         lw $s4, 0($t1)#temp = v[upper]
        
         sll $t0, $a1, 2 
 	add $t2, $a0, $t0 #adressen till v[a]
 	lw $t3, 0($t2)#t3 = v[a]
-	sw $t3, 0($t1)#v[upper] = v[a];
-	sw $s4, 0($t2) #v[a] = temp;
+	
+	sll $t0, $s3, 2
+	addu $t0, $a0, $t0
+	sw $t3, 0($t0)#v[upper] = v[a];
+	
+	sll $t0, $a1, 2
+	addu $t0, $a0, $t0
+	sw $s4, 0($t0) #v[a] = temp;
 	
 	move $v0, $s3 #returnera upper
 	jr $ra
-	
+
+#-----------------------------------------------------------Quicksort-----------------------------------------------
 quicksort:
 
 	#Förbreder stacken
 	subu $sp, $sp, 24 #går ner i stacken
 	sw $ra, 16($sp) #retur adress för att kunna hoppa tillbaka till Main
-	sw $s0, 12($sp) # k
+	sw $s7, 12($sp) # k
 	sw $a0, 8($sp) #vek
-	sw $a2, 4($sp) #b  ,b = antal - 1, som är 9 i början
-	sw $a1, 0($sp) #a  ,a = 0 i början
+	sw $a2, 4($sp) #a  ,a = 0 i början
+	sw $a1, 0($sp) #b  ,b = antal - 1, som är 9 i början
 	
-	move $t1, $a1
-	move $t2, $a2
+	move $t1, $a2
+	move $t2, $a1
 	bge $t1,$t2, endif
 	lw $a0, 8($sp) #v[]
-	lw $a1, 0($sp) #a
-	lw $a2, 4($sp) #b
+	lw $a2, 0($sp) #a
+	lw $a1, 4($sp) #b
 	jal partition
 	
-	move $s0, $v0
-	addi $s0, $s0, -1
+	move $s7, $v0
+	addi $s7, $s7, -1
 	lw $a0, 8($sp)
-	lw $a1, 0($sp)
-	move $a2, $s0
+	lw $a2, 0($sp)
+	move $a1, $s7
 	jal quicksort
 	
 	
-	addi $s0, $s0, 1
+	addi $s7, $s7, 1
 	lw $a0, 8($sp)
-	move $a1, $s0
-	lw $a2, 4($sp)
+	move $a2, $s7
+	lw $a1, 4($sp)
 	jal quicksort
 	
 	
 endif: 
 	lw $ra, 16($sp) #hämtar retur addressen från stacken
-	lw $s0, 12($sp)
+	lw $s7, 12($sp)
 	addu $sp, $sp, 24  #går upp i stacken
 	jr $ra
